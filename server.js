b589ce7e9a7a18519e78c639cd98a7d4114e2c63
AlexOleynyk <alex.oleynyk@gmail.com>
2016-12-10 16:21:04 +0000

    mail chenge

--- server.js
+++ server.js
@@ -705,11 +705,11 @@ app.post('/invitefriend', function(req, res) {
         // '<a href="http://brainny.herokuapp.com/?ref=' + req.session.user.username + '" style="color: #2B223B"> Зарегестрироваться </a>.</p>' +
         // '<p>Если с ссылкой что-то не так, просто скопируй этот адрес и вставь его в адресную строку браузера: </p>' + 
         // '<p> brainny.herokuapp.com?ref=' + req.session.user.username + '</p>'   // html body 
-        html: '<table style="   font-family: sans-serif;    line-height: 28px;    background: #131728;    color: white;    width: 100%;'+
+        html: '<table cellpadding="20" style="   font-family: sans-serif;    line-height: 28px;    background: #342642;    color: white;    width: 100%;'+
     'text-align: center;    padding: 50px; ">  <tr>'+
     '<td><h2>Привет</h2></td>'+
-    '</tr>  <tr> <td><p>Пользователь '+ req.session.user.username +' приглашает тебя тренировать мозг в сервисе Brainny. <br> Ты можешь зарегистрироваться по этой ссылке: </p></td>'+
-    '</tr>  <tr>    <td>      <a class="btn" style="background: #2fb16f;    display: inline-block;    margin-top: 20px;' + 
+    '</tr>  <tr> <td><p>Пользователь '+ req.session.user.username +' приглашает тебя тренировать мозг в сервисе Brainny. <br> Ты можешь зарегистрироваться нажав на кнопку ниже: </p></td>'+
+    '</tr>  <tr>    <td>      <a class="btn" style="background: #2fb16f;    display: inline-block;    margin-top: 20px; margin-bottom: 50px;' + 
     'padding: 14px 22px;    color: white;    text-transform: uppercase;    border-radius: 50px;    text-decoration: none;'+
     'box-shadow: 0 0 30px 0 #2fb16f;" href="http://brainny.herokuapp.com/?ref=' + req.session.user.username + '"> Зарегистрироваться </a>    </td>  </tr></table>'        // html body 
       };
